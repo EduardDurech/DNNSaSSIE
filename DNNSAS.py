a35a781275abd3737deeb353b09a4fcb8596c298
EduardDurech <39579228+EduardDurech@users.noreply.github.com>
2019-01-09 22:35:26 -0800

    Add files via upload

---
+++ DNNSAS.py
@@ -0,0 +1,123 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+from PIL import Image
+from skimage.io import imread, imshow, imread_collection, concatenate_images
+
+from keras.models import Model, load_model
+from keras.layers import Input
+from keras.layers.core import Dropout, Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
+
+import tensorflow as tf
+
+
+dat_set = "C:/Users/BORG-AO-Ref/Documents/CommercialSaSSIE/train/dat/"
+#dat_trgt = "C:/Users/BORG-AO-Ref/Documents/CommercialSaSSIE/train/gt_res/"
+dir_titles = next(os.walk(dat_set))[1]
+#train_ids = []
+temp_list = []
+temp_list1 = []
+set_len = 0
+
+cur_data = "deep"
+cur_trgt = "OPL"
+
+ext = (cur_data+".bmp", cur_data+".tif")
+
+"""
+for dir_adr in dir_titles:
+	for adr in next(os.walk(dat_set+"/"+dir_adr))[2]:
+		train_ids.append(dir_adr+"-"+adr)
+"""
+
+X_train = np.zeros((len(dir_titles), 10, 512, 512))
+Y_train = np.zeros((len(dir_titles), 512, 512))
+
+for ind, path in tqdm(enumerate(dir_titles), total=len(dir_titles)):
+	#dirs = os.listdir(path)
+	i = 0
+	for picture in next(os.walk(dat_set+"/"+path))[2]:
+		if picture.endswith(ext):
+			im = Image.open(dat_set+"/"+path+"/"+picture)
+			imResize = im.resize((512,512))
+			X_train[ind][i] = imResize
+			i += 1
+
+		#if picture.startswith("final_"+cur_trgt):
+			im = Image.open(dat_set+"/"+path+"/"+picture)
+			imResize = im.resize((512,512))
+			Y_train[ind] = imResize
+print("Finished resizing")
+
+"""
+inputs = Input((512, 512, 1))
+s = Lambda(lambda x: x / 255) (inputs)
+
+c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (s)
+c1 = Dropout(0.1) (c1)
+c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c1)
+p1 = MaxPooling2D((2, 2)) (c1)
+
+c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p1)
+c2 = Dropout(0.1) (c2)
+c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c2)
+p2 = MaxPooling2D((2, 2)) (c2)
+
+c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p2)
+c3 = Dropout(0.2) (c3)
+c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c3)
+p3 = MaxPooling2D((2, 2)) (c3)
+
+c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p3)
+c4 = Dropout(0.2) (c4)
+c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c4)
+p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+
+c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p4)
+c5 = Dropout(0.3) (c5)
+c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c5)
+
+u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
+u6 = concatenate([u6, c4])
+c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u6)
+c6 = Dropout(0.2) (c6)
+c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c6)
+
+u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
+u7 = concatenate([u7, c3])
+c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u7)
+c7 = Dropout(0.2) (c7)
+c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c7)
+
+u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
+u8 = concatenate([u8, c2])
+c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u8)
+c8 = Dropout(0.1) (c8)
+c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c8)
+
+u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
+u9 = concatenate([u9, c1], axis=3)
+c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
+c9 = Dropout(0.1) (c9)
+c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c9)
+
+outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+
+model = Model(inputs=[inputs], outputs=[outputs])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[mean_iou])
+model.summary()
+
+earlystopper = EarlyStopping(patience=5, verbose=1)
+checkpointer = ModelCheckpoint('model-dsbowl2018-1.h5', verbose=1, save_best_only=True)
+results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=16, epochs=50, 
+                    callbacks=[earlystopper, checkpointer])
+"""
